bf074112317f7ff6b06ef89ddcbad613b2a49e8a
supabase12345-boop <supabase12345@gmail.com>
2026-02-14 20:31:31 +0300

    Add files via upload

---
+++ supabase.js
@@ -0,0 +1,7 @@
+
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
+
+export const supabase = createClient(
+  "https://izfsxwgnccziswyyedxo.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml6ZnN4d2duY2N6aXN3eXllZHhvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzEwNzk0MDIsImV4cCI6MjA4NjY1NTQwMn0.Lgyjy6PGaYbfYLtiyR3yvVn7IU4jnkTVAUVHAMfKW3Y"
+);
